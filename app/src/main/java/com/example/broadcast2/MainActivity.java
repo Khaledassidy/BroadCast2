278cbeab4934680d703c1b9e09d029755c071d5a
khaled Assidi <Kaa502@students.bau.edu.lb>
2024-08-29 02:22:47 +0300

    first commit

---
+++ app/src/main/java/com/example/broadcast2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.broadcast2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(v->{
+            Intent intent=new Intent();
+            intent.setAction("com.example.broadcasts");
+            sendBroadcast(intent);
+        });
+
+
+
+    }
+}
